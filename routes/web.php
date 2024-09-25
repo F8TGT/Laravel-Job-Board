56e086469e68e589cd181e118505349f1206aa95
Edward <78192184+F8TGT@users.noreply.github.com>
2024-09-25 08:21:10 -0600

    - Created 2 new components job-card and link-button. - added a new view to check an individual job.

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('', fn() => to_route('job.index'));
-Route::resource('jobs', JobController::class)->only(['index']);
+Route::get('', fn() => to_route('jobs.index'));
+Route::resource('jobs', JobController::class)->only(['index', 'show']);
