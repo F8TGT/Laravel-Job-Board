453b455c66b0552d27e0b425d30e9c00b6c46772
Edward <78192184+F8TGT@users.noreply.github.com>
2024-11-20 07:59:14 -0600

    → Clean the code.

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\JobRequest;
 use App\Models\Job;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 
 class MyJobController extends Controller
